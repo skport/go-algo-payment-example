b423b5445834a444cb4849b5bde9f5b0666a5142
skport <45557654+skport@users.noreply.github.com>
2023-11-26 01:06:59 +0900

    test

--- go.mod
+++ go.mod
@@ -1,6 +1,8 @@
 module github.com/skport/go-algorand-payment
 
-go 1.20
+go 1.21
+
+toolchain go1.21.0
 
 require (
 	github.com/algorand/go-algorand-sdk v1.24.0
--- main.go
+++ main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"context"
 	"encoding/base64"
+	"encoding/hex"
 	"fmt"
-	// "log"
-	"strings"
-	"bufio"
+	"log"
 	"os"
+	"strings"
 
 	"crypto/sha512"
 	"encoding/base32"
@@ -18,34 +19,42 @@ import (
 	"github.com/algorand/go-algorand-sdk/client/v2/algod"
 	"github.com/algorand/go-algorand-sdk/crypto"
 	"github.com/algorand/go-algorand-sdk/encoding/msgpack"
-	// "github.com/algorand/go-algorand-sdk/transaction"
 	"github.com/algorand/go-algorand-sdk/types"
 )
 
 const (
-	minFee      = 1000
-	genesisId   = "testnet-v1.0"
+	// 最低手数料 1000 MicroAlgos
+	minFee = 1000
+	// ネットワークのバージョン
+	genesisId = "testnet-v1.0"
+	// 最初のブロックハッシュ (Base64)
 	genesisHash = "SGO1GKSzyE7IEPItTxCByw9x8FmnrCDexi9/cOUJOiI="
+	// 開始ラウンド（開始ブロックナンバー）
+	lastRound = 100000
 )
 
 func main() {
+	// 1. 送金元のアカウント作成
 	FromAccount := crypto.GenerateAccount()
 	fmt.Println("Algorand FromAddress", FromAccount.Address.String())
+	fmt.Println("Algorand FromAddress PrivateKey", hex.EncodeToString(FromAccount.PrivateKey))
+	fmt.Println("Algorand FromAddress PublicKey", hex.EncodeToString(FromAccount.PublicKey))
 
+	// 2. 送金先のアドレス構造体を作成
 	ToAccount := crypto.GenerateAccount()
-	fmt.Println("Algorand TOAddress", ToAccount.Address.String())
+	fmt.Println("Algorand ToAddress", ToAccount.Address.String())
 
-	// Validate params for transaction
-	var ghDigest  types.Digest
+	// GenesisHashを byte[] に変換
+	var ghDigest types.Digest
 	copy(ghDigest[:], byteFromBase64(genesisHash))
 
-	// TRANSACTION_PAYMENT_CREATE
+	// 3. 署名前のトランザクション作成
 	unSignedTx := types.Transaction{
 		Type: "pay",
 		Header: types.Header{
 			Sender:      FromAccount.Address,
-			FirstValid:  types.Round(10000),
-			LastValid:   types.Round(10000 + 1000),
+			FirstValid:  types.Round(lastRound),
+			LastValid:   types.Round(lastRound + 1000),
 			Note:        nil,
 			GenesisID:   genesisId,
 			GenesisHash: ghDigest,
@@ -58,25 +67,24 @@ func main() {
 	}
 	unSignedTx.Fee = types.MicroAlgos(minFee)
 
-	// Sign the encoded transaction
-
 	// Encode the transaction as msgpack
 	encodedTx := msgpack.Encode(unSignedTx)
 	// Prepend the hashable prefix
 	msgParts := [][]byte{[]byte("TX"), encodedTx}
 	toBeSigned := bytes.Join(msgParts, nil)
 
+	// 4. 署名の作成
 	signature := ed25519.Sign(FromAccount.PrivateKey, toBeSigned)
-	// Copy the resulting signature into a Signature, and check that it's
-	// the expected length
+	// Copy the resulting signature into a Signature,
+	// and check that it's the expected length
 	var algoSignature types.Signature
 	n := copy(algoSignature[:], signature)
 	if n != len(algoSignature) {
-		fmt.Errorf("Failed to copy signature")
+		log.Fatal("Failed to copy signature")
 	}
 	fmt.Println("Signature", algoSignature)
 
-	// Create the SignedTxn
+	// 5. 署名済みトランザクションの作成
 	stx := types.SignedTxn{
 		Sig: algoSignature,
 		Txn: unSignedTx,
@@ -85,33 +93,34 @@ func main() {
 	// Encode the SignedTxn
 	stxBytes := msgpack.Encode(stx)
 
-	// Create the Transaction ID
+	// 6. TxIDの確認
 	txid := txIDFromRawTxnBytesToSign(toBeSigned)
 	fmt.Println("Transaction ID", txid)
 
-	// Waiting for user input
-	fmt.Print("continue? (y/n) >")
-	scanner := bufio.NewScanner(os.Stdin)	
+	// ブロードキャストする前に送信内容を確認するため、ユーザーの入力を待つ
+	fmt.Print("\nContinue? (y/n) >")
+	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
 	in := scanner.Text()
 
-	// Broadcast the transaction to the network
 	switch in {
 	case "y":
+		// 7. "y" を入力したら、署名済みトランザクションデータをブロードキャスト
 		fmt.Println("Sending transaction...")
 		pendingTxID, err := sendTransaction(stxBytes)
 		if err != nil {
-			fmt.Printf("Failed to send transaction: %s\n", err)
+			log.Fatalf("Failed to send transaction: %s\n", err)
 		}
 		fmt.Printf("Pending Transaction ID: %s\n", pendingTxID)
 	default:
+		// y 以外の入力なら終了
 		break
 	}
 }
 
 func sendTransaction(stxBytes []byte) (string, error) {
-	var algodAddress = "https://testnet-api.algonode.cloud"
-	var algodToken = strings.Repeat("a", 64)
+	algodAddress := "https://testnet-api.algonode.cloud"
+	algodToken := strings.Repeat("a", 64)
 	algodClient, _ := algod.MakeClient(
 		algodAddress,
 		algodToken,
@@ -119,7 +128,6 @@ func sendTransaction(stxBytes []byte) (string, error) {
 
 	pendingTxID, err := algodClient.SendRawTransaction(stxBytes).Do(context.Background())
 	if err != nil {
-		fmt.Printf("failed to send transaction: %s\n", err)
 		return "", err
 	}
 
@@ -131,8 +139,7 @@ func byteFromBase64(s string) []byte {
 	return b
 }
 
-func txIDFromRawTxnBytesToSign(toBeSigned []byte) (txid string) {
+func txIDFromRawTxnBytesToSign(toBeSigned []byte) string {
 	txidBytes := sha512.Sum512_256(toBeSigned)
-	txid = base32.StdEncoding.WithPadding(base32.NoPadding).EncodeToString(txidBytes[:])
-	return
+	return base32.StdEncoding.WithPadding(base32.NoPadding).EncodeToString(txidBytes[:])
 }
